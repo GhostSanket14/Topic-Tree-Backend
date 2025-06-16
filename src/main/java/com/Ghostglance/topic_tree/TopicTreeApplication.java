89140e5750360a198fea49d25194ba2afb78b2ab
sanket h <sanketghinchanal@gmail.com>
2025-06-17 00:20:22 +0530

    Initial backend commit

---
+++ src/main/java/com/Ghostglance/topic_tree/TopicTreeApplication.java
@@ -0,0 +1,15 @@
+package com.Ghostglance.topic_tree;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopicTreeApplication {
+
+	// Here we go again
+	
+	public static void main(String[] args) {
+		SpringApplication.run(TopicTreeApplication.class, args);
+	}
+
+}
